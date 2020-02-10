d9ee701c480dca565c04a6b879347a2e1f31b934
BlueC0C0 <BlueC0C0@users.github.com>
2020-02-10 21:49:43 +0100

    Update Fenetre.java

--- calculatrice/src/calculatrice/Fenetre.java
+++ calculatrice/src/calculatrice/Fenetre.java
@@ -2,13 +2,16 @@ package calculatrice;
 
 
 import java.awt.Color;
+
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Fenetre extends JFrame{
+public class Fenetre extends JFrame implements ActionListener{
 	
 	private JPanel pan = new JPanel();
 	private JButton b0 = new JButton("0");  
@@ -29,6 +32,13 @@ public class Fenetre extends JFrame{
 	private JButton b07 = new JButton("=");
 	private JButton b08 = new JButton(",");
 	private JButton b06 = new JButton("resultat");
+	
+	private double op1 = 0;
+	private double op2 = 0;
+	private double resultat;
+	private String operateur = "@";
+	private boolean virg = false;
+	private boolean res = false;
 	
 	
 	
@@ -60,6 +70,24 @@ public class Fenetre extends JFrame{
 
 	    this.setLayout(null);
 	    this.setVisible(true);
+	    b0.addActionListener(this);
+	    b1.addActionListener(this);
+	    b2.addActionListener(this);
+	    b3.addActionListener(this);
+	    b4.addActionListener(this);
+	    b5.addActionListener(this);
+	    b6.addActionListener(this);
+	    b7.addActionListener(this);
+	    b8.addActionListener(this);
+	    b9.addActionListener(this);
+	    b01.addActionListener(this);
+	    b02.addActionListener(this);
+	    b03.addActionListener(this);
+	    b04.addActionListener(this);
+	    b05.addActionListener(this);
+	    b06.addActionListener(this);
+	    b07.addActionListener(this);
+	    b08.addActionListener(this);
 	    this.add(b0);
 	    this.add(b1);
 	    this.add(b2);
@@ -79,15 +107,17 @@ public class Fenetre extends JFrame{
 	    this.add(b07);
 	    this.add(b08);
 	    this.run();
+	    
+	    
 	  }
 	private void run()
 	{
 		for(;;)
 		{
 			int xRes, yRes;
-			xRes = this.getWidth()-5*6;
+			xRes = this.getWidth()-5*6-10;
 			xRes/=3.5;
-			yRes = this.getHeight()-5*7-20;
+			yRes = this.getHeight()-5*7-30;
 			yRes/=4.5;
 			this.setBouton(b0, 10+xRes, 25+7*(yRes/2), xRes, yRes);//
 			
@@ -110,7 +140,8 @@ public class Fenetre extends JFrame{
 		    this.setBouton(b05, 20+3*xRes, 30+5*(yRes/2), xRes/2, yRes/2);//
 		    this.setBouton(b07, 20+3*xRes, 35+6*(yRes/2), xRes/2, yRes/2);//
 		    this.setBouton(b08, 5, 25+7*(yRes/2), xRes, yRes);//
-		    this.setBouton(b06, 5, 5, this.getWidth()-10, yRes/2);//
+		    this.setBouton(b06, 5, 5, this.getWidth()-25, yRes/2);//
+		    this.setContent();
 		}
 	}
 	
@@ -118,6 +149,107 @@ public class Fenetre extends JFrame{
 	{
 		bouton.setBounds(x, y, w, h);
 	}
-	
 
+	@Override
+	public void actionPerformed(ActionEvent arg) {
+		int val = -1;
+		this.res= false;
+		if(arg.getSource()== b0)
+			val = 0;
+		if(arg.getSource()== b1)
+			val = 1;
+		if(arg.getSource()== b2)
+			val = 2;
+		if(arg.getSource()== b3)
+			val = 3;
+		if(arg.getSource()== b4)
+			val = 4;
+		if(arg.getSource()== b5)
+			val = 5;
+		if(arg.getSource()== b6)
+			val = 6;
+		if(arg.getSource()== b7)
+			val = 7;
+		if(arg.getSource()== b8)
+			val = 8;
+		if(arg.getSource()== b9)
+			val = 9;
+		
+		if(val != -1)
+		{
+			if(this.operateur != "@")
+			{
+				this.op2 = this.op2*10 + val;
+				System.out.println(this.op2);
+			}
+			else
+			{
+				this.op1 = this.op1*10 + val;
+			}
+		}
+		else
+		{
+			if(arg.getSource() == b01)
+				this.operateur = "+";
+			if(arg.getSource() == b02)
+				this.operateur = "-";
+			if(arg.getSource() == b03)
+				this.operateur = "/";
+			if(arg.getSource() == b04)
+				this.operateur = "*";
+		}
+		
+		if(arg.getSource() == b07)
+		{
+			if(this.operateur == "+")
+				this.resultat = this.op1 + this.op2;
+			if(this.operateur == "-")
+				this.resultat = this.op1 - this.op2;
+			if(this.operateur == "/")
+				this.resultat = this.op1 / this.op2;
+			if(this.operateur == "*")
+				this.resultat = this.op1 * this.op2;
+			this.res = true;
+			
+			this.operateur = "@";
+			this.op1 = 0;
+			this.op2 = 0;
+		}
+		
+		if(arg.getSource() == b05)
+		{
+			this.op1 = 0;
+			this.op2 = 0;
+			this.operateur = "@";
+			this.resultat = 0;
+			b06.setText("");
+		}
+		
+		
+		
+	}
+	
+	private void setContent()
+	{
+		
+		if(this.operateur == "@" && !this.res)
+		{
+			if(this.op1 == 0)
+				b06.setText("");
+			else
+				b06.setText("" + this.op1);
+		}
+		else if(!this.res)
+		{
+			if(this.op2 == 0)
+				b06.setText("" + this.op1 +" "+ this.operateur);
+			else
+				b06.setText("" + this.op1 +" "+ this.operateur +" "+ this.op2);
+		}
+		
+		if(this.res)
+		{
+			b06.setText("" + this.resultat);
+		}
+	}
 }
